510639364a7235d7eda6bd7a59bd4f3a99cd04e4
Rakhimov Kamran <rakhimovkamran@gmail.com>
2021-12-09 13:47:04 +0500

    | Project | Fix | Active Link Behavior

--- assets/js/index.js
+++ assets/js/index.js
@@ -69,12 +69,12 @@ let newSwiper = new Swiper('.new-swiper', {
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
-    const scrollY = window.pageYOffset;
+    const scrollY = window.scrollY;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id');
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id');
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
